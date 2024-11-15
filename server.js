694dd36a51d024ed2f989886218fa478da5c1bbc
Mazemk2 <Mazemk2@googlemail.com>
2024-11-15 07:27:16 +0000

    Node Module, sowie aktivierung der Anmeldungs- routine, überarbeitung Dumb der Datenbank

--- server.js
+++ server.js
@@ -9,3 +9,16 @@ const server = http.createServer((req, res) => {
 server.listen(3000, () => {
     console.log('Server running at http://localhost:3000/');
 });
+//
+// server.mjs
+// import { createServer } from 'node:http';
+// const server = createServer((req, res) => {
+//  res.writeHead(418, { 'Content-Type': 'text/plain' });
+//  res.end('Hello World!\n');
+//});
+
+// starts a simple http server locally on port 3000
+// server.listen(3000, '127.0.0.1', () => {
+//    console.log('Listening on 127.0.0.1:3000');
+// });
+  // run with `node server.mjs`
